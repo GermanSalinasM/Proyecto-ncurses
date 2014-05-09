0932b97183bb4b4e0702956a08fb1dd5d05f5d62
GermanSalinasM <germansalinasmiranda@gmail.com>
2014-05-09 15:40:26 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main()
     int y = 5;
     int ch;
     initscr();
-    start_color();
+    start_color();//da color
     use_default_colors();
     curs_set(0);
     keypad(stdscr, TRUE);
@@ -14,30 +14,30 @@ int main()
     while (1)
     {
         init_pair(1,COLOR_GREEN,COLOR_RED);
-        attron(COLOR_PAIR(1));
+        attron(COLOR_PAIR(1));//aplica color
         mvprintw(y, x, "O");
         ch = getch();
-        if (ch == KEY_DOWN)
+        if (ch == KEY_DOWN)//se mueve hacia abajo
         {
             y++;
             clear();
         }
-        if (ch == KEY_UP)
+        if (ch == KEY_UP)//se mueve hacia arriba
         {
             y--;
             clear();
         }
-        if (ch == KEY_LEFT)
+        if (ch == KEY_LEFT)//se mueve hacia izq.
         {
             x--;
             clear();
         }
-        if (ch == KEY_RIGHT)
+        if (ch == KEY_RIGHT)//se mueve hacia derecha
         {
             x++;
             clear();
         }
-        attroff(COLOR_PAIR(1));
+        attroff(COLOR_PAIR(1));//deja de aplicar color
         refresh();
     }
 
